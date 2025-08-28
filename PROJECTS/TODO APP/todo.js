79e8932753cbee290b10640037307d1bd7e7e8bc
TOMAR-PARAKRAM <tomarparakram17@gmail.com>
2025-08-28 08:11:10 +0530

    empty

--- PROJECTS/TODO APP/todo.js
+++ PROJECTS/TODO APP/todo.js
@@ -1,1 +1,30 @@
-// step 1 => 
+// step 1 => stop the reloading of the window 
+let addbtn = document.querySelector(".addbtn")
+addbtn.addEventListener("submit", function(e){
+    e.preventDefault()
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+kdnnsdvns
+
+
+
+// step 1 => get access what you have written in the input field
+
+// step 1 => // create the similar element like 
