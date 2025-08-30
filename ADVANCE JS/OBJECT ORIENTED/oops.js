4ed53eb42dc284bddba87e27496ecbc7ca987753
TOMAR-PARAKRAM <tomarparakram17@gmail.com>
2025-08-30 16:23:02 +0530

    learning classes

---
+++ ADVANCE JS/OBJECT ORIENTED/oops.js
@@ -0,0 +1,36 @@
+// // start learning oops 
+// // this is constructor function 
+// function Laptop (company,processor, ram ,rom , color ){
+//     this.company  = company
+//     this.processor = processor
+//     this.ram =ram 
+//     this.rom  = rom 
+//     this.color = color 
+// }
+// laptop.prototype.battery = " 4hrs " // now sabhi new instances ya objects ke paas ye  field chali jatai hai jo ki fix hai 
+
+// let laptop1  = new Laptop("asus" ,"i5" , "16" , "512" , "blue"); 
+// let laptop2  = new Laptop("hp" , "i7" ,"8gb" ,  "265gb" , "silver" )
+
+
+// classes 
+// class Createpencil{
+//     constructor(user , color , price ,company  ){
+//         this.user = user 
+//         this.color  = color 
+//         this.price  = price 
+//         this.compant  = company 
+//     }
+
+// write(text){
+//     let h1 = document.createElement("h1")
+//     h1.style.color = this.color
+//     h1.innerHTML = text 
+//     document.body.appendChild(h1)
+// }
+
+// }
+// let p1 = new Createpencil("tom" , "blue" , "20rs" ,  'apsra' ); 
+// let p2 = new Createpencil ("rey" , "red" , "15rs" , "doms") ;
+
+
---
+++ ADVANCE JS/THIS  KEYWORD/practice /proj1.js
@@ -0,0 +1,7 @@
+// write the psudo code 
+//step1=> ek object lo userManager , 
+// step 2 , => create some properties like users , jo ki ek blank array hoga 
+// step3  => create a method add user , 
+// create a method named rmove user 
+
+// ek init method banao or usme prevent karo , fir 
