302358ff76df0a3f963c55ef672ef6bdfb8f7eb8
TOMAR-PARAKRAM <tomarparakram17@gmail.com>
2025-09-01 04:47:44 +0530

    at the ending of this course 2-4 topic left

--- PROJECTS/TODO APP/todo.js
+++ PROJECTS/TODO APP/todo.js
@@ -1,30 +1,21 @@
-// step 1 => stop the reloading of the window 
+/// step 1 => get access what you have written in the input field/
+//  step 1 => stop the reloading of the window 
 let addbtn = document.querySelector(".addbtn")
-addbtn.addEventListener("submit", function(e){
+let input = document.querySelector(".userinp")
+addbtn.addEventListener("click", function(e){
     e.preventDefault()
+    console.log(input.value);
+    // step 1 => // create the similar element like tgs
+    let container = document.createElement("div") 
+    let tasks = document.createElement("li")
+    let buttons = document.createElement("div")
+    let donebtn = document.createElement("button")
+    let delbtn = document.createElement("button")
+
+    buttons.appendChild(donebtn)
+    buttons.appendChild(delbtn)
+    tasks.appendChild(buttons)
+    container.appendChild(tasks)
 })
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-kdnnsdvns
-
-
-
-// step 1 => get access what you have written in the input field
-
-// step 1 => // create the similar element like 
---
+++ X - O GAME/script.js
@@ -0,0 +1,12 @@
+let cell =  document.querySelectorAll(".cell");  
+let body = document.querySelector("body")
+
+body.addeventlistner("change" , function(){
+    e.preventdefault()
+    
+})
+
+cell.addeventlistner("click" ,function(){
+    cell.innerText = 
+})
+
