61a76f9d5d67105c9f9dda3435c52a930151c57b
TOMAR-PARAKRAM <tomarparakram17@gmail.com>
2025-09-13 17:05:44 +0530

    working of the async code

--- ADVANCE JS/ASYNC JS/asy.js
+++ ADVANCE JS/ASYNC JS/asy.js
@@ -1,18 +1,24 @@
 // SYNC => 
-
-let sync = function(){
-    console.log("1");
-    console.log("2")
-
-}
-sync()
+// matlab cheezin line wise hogi 
+// let sync = function(){
+//     console.log("1");
+//     console.log("2")
+// }
+// sync()
 
 
 // ASYNC => 
+    console.log("before");
+    
+let asy = function (){
+    setTimeout(function(){
+        console.log("hello world ")
+    }, 3000);
+}
+asy()
+console.log("after");
 
-    let aysncfnc = function(){
-        
-    }
-
+//  now this is how async works 
 
 // EVENT LOOP 
+
