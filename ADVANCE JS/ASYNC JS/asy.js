d8cbb040559415ddd5106362e2fd2ba847c4af0e
TOMAR-PARAKRAM <tomarparakram17@gmail.com>
2025-09-13 16:17:45 +0530

    learning and defining async

---
+++ ADVANCE JS/ASYNC JS/asy.js
@@ -0,0 +1,18 @@
+// SYNC => 
+
+let sync = function(){
+    console.log("1");
+    console.log("2")
+
+}
+sync()
+
+
+// ASYNC => 
+
+    let aysncfnc = function(){
+        
+    }
+
+
+// EVENT LOOP 
