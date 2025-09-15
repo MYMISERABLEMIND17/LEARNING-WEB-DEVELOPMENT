0d83d9c08e665487e27afdeb588c567cd304ee42
TOMAR-PARAKRAM <tomarparakram17@gmail.com>
2025-09-15 23:42:47 +0530

    last random noe goingto cohort for promises and and async

--- ADVANCE JS/ASYNC JS/asy.js
+++ ADVANCE JS/ASYNC JS/asy.js
@@ -8,17 +8,40 @@
 
 
 // ASYNC => 
-    console.log("before");
+//     console.log("before");
     
-let asy = function (){
-    setTimeout(function(){
-        console.log("hello world ")
-    }, 3000);
-}
-asy()
-console.log("after");
+// let asy = function (){
+//     setTimeout(function(){
+//         console.log("hello world ")
+//     }, 3000);
+// }
+// asy()
+// console.log("after");
 
-//  now this is how async works 
+//  now this is how async works first the sync code work and if there is any delay then only async wait , sync , will run as it is . 
 
-// EVENT LOOP 
 
+// fetch 
+// axious 
+// Promises
+// setTimeout
+// setinterval
+// ye sab request ko bhejne kaaa kaam karte hain 
+
+
+// then catch 
+// callbacks 
+// async await
+// ye sab request ke answer ko catch karte hain 
+
+
+// call backs 
+// call back humesha ek function hota hai , ye sirf tab chalta hai jab async code ka completion ho jata hain 
+
+
+// promises 
+let x = new Promise(function(res , rej ){
+    return res("idhar aao ")
+})
+
+x.then(res)
