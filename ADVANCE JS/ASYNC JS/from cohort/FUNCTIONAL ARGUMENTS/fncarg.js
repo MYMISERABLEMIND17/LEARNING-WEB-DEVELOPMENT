b09a55ef8ae95ef1d09ba89282ac8c7b553e0392
TOMAR-PARAKRAM <tomarparakram17@gmail.com>
2025-09-16 22:29:48 +0530

    random , learning the functional argumets

---
+++ ADVANCE JS/ASYNC JS/from cohort/FUNCTIONAL ARGUMENTS/fncarg.js
@@ -0,0 +1,42 @@
+// function sum(a, b) {
+//     return a + b;
+// }
+
+// function multiply(a, b) {
+//   return a * b;
+// }
+
+// function subtract(a, b) {
+//     return a - b;
+// }
+
+// function divide(a, b) {
+//     return a / b;
+// }
+// console.log(sum(1, 2))
+
+
+/* functional argument => basically passing the function as an argument is called fumctionalargument */
+
+// Passing in what needs to be done as an argument.
+function sum(a, b) {
+    return a + b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function doOperation(a, b, op) {
+    return op(a, b)
+}
+    
+console.log(doOperation(1, 2, sum))
