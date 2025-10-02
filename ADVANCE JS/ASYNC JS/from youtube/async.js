86584a94ae1f5cdddc5ccb22f98e150f1ef67757
TOMAR-PARAKRAM <tomarparakram17@gmail.com>
2025-10-02 13:00:16 +0530

    learning callbacks

---
+++ ADVANCE JS/ASYNC JS/from youtube/async.js
@@ -0,0 +1,21 @@
+// console.log(" one ") ;
+// console.log(" two ") ;
+// console.log(" three ") ;
+
+// setTimeout(() => {
+//     console.log(" four ") ;
+// }, 2000);
+// console.log(" five ") ;
+
+
+// callbacks 
+
+// function multi(a , b ){
+//     console.log (a * b) ;
+// }
+// function calculate (a, b , multicallback ){
+//     multicallback(a , b )
+// }
+// calculate(3, 5 , multi)
+
+
