d8a8e580cf0414e08102e2682ffb6218026610b4
TOMAR-PARAKRAM <tomarparakram17@gmail.com>
2025-10-02 14:01:57 +0530

    completing the promises

--- ADVANCE JS/ASYNC JS/from youtube/async.js
+++ ADVANCE JS/ASYNC JS/from youtube/async.js
@@ -19,3 +19,55 @@
 // calculate(3, 5 , multi)
 
 
+// call back hell 
+
+// function getData(dataId, nextData){
+//     setTimeout(()=>{
+//         console.log("heloo => " + dataId);
+//         if(nextData){
+//             nextData();
+//         }
+        
+//     }, 3000); 
+// }
+
+// getData(1 , ()=>{
+//     getData(2, ()=>{
+//         getData(3 ,()=>{
+//             getData(4)
+//         })
+//     })
+// })// this is how callback works ,, this is called callback hell 
+
+
+
+
+// promises 
+// syntax =>  let promise = new Promise((resolve , reject )=>{.......
+// })
+
+// let promise = new Promise((resolve ,reject) =>{
+//     console.log(" hi i am a promise");
+//     reject("error")
+// })
+
+// working of promises 
+// function getData(dataId, nextData){
+    
+//     return new Promise((resolve , reject)=>{
+//         setTimeout(()=>{
+//         console.log("heloo =>" + dataId);
+//         resolve("success")
+//         if(nextData){
+//             nextData();
+//         }
+//         }, 7000);    
+
+//     });
+// }
+
+
+// .then and .catch
+
+
+
