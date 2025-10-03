7a77e6541c23ba7c5fcf69d6b658db5822289f24
TOMAR-PARAKRAM <tomarparakram17@gmail.com>
2025-10-03 18:25:51 +0530

    just building up the project 1

--- GSAP/PROJECT1/script.js
+++ GSAP/PROJECT1/script.js
@@ -1,14 +1,21 @@
-
-
-let main = document.querySelector("#main")
-let cursor = document.querySelector("#cursor")
-
+let main = document.querySelector("#main"); 
+let cursor = document.querySelector("#cursor") ; 
+let image = document.querySelector("#image") ; 
 main.addEventListener("mousemove",function(dets){
-    gsap.to("#cursor" ,{
+    gsap.to(cursor ,{
         x:dets.x ,
-        y:dets.y
+        y:dets.y, 
+        scale:0.5
     })
-    
 })
 
-
+image.addEventListener("mousemove",function(){
+    gsap.to(cursor , {
+        scale:3
+    })
+})
+image.addEventListener("mouseleave",function(){
+    gsap.to(cursor , {
+        scale:0.5 
+    })
+})
