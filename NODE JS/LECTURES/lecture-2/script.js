526292c3b1eb44a87ecaea5dec3dfa620c58b7ae
TOMAR-PARAKRAM <tomarparakram17@gmail.com>
2025-10-05 01:30:06 +0530

    modified a file

--- NODE JS/LECTURES/lecture-2/script.js
+++ NODE JS/LECTURES/lecture-2/script.js
@@ -1,11 +1,20 @@
 const fs = require('fs');
 
-fs.writeFile("trial.txt" , "helooo this is a trial",function(err){
-    if (err){
-        console.log(err);
-    }
-    else{
-        console.log("complete");
+// fs.writeFile("trial.txt" , "helooo this is a trial",function(err){
+//     if (err){
+//         console.log(err);
+//     }
+//     else{
+//         console.log("complete");
         
-    }
+//     }
+// })
+
+
+// fs.appendFile("trial.txt" ," this is the append part " ,function(err){
+// })
+
+fs.rename("trial.txt" ,"modifiedtrial.txt" , function(err){
+
 })
+
