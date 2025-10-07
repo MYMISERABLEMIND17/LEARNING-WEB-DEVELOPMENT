e904ded69126162c0dc3cc60b42aff42ce7096f6
TOMAR-PARAKRAM <tomarparakram17@gmail.com>
2025-10-07 08:57:29 +0530

    start learning react

--- REACT/BASICS /script.js
+++
@@ -1,9 +0,0 @@
-// console.log(React);
-
-var h1 = React.createElement ("h1" , null , " hello from react ")
-
-var parent = document.querySelector("#parent")
-
-var root = ReactDOM.createRoot(parent)
-
-root.render(h1)     
