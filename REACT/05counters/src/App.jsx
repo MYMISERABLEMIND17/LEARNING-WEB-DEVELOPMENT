f4e8dee6bfb9269a8da6a0bc33f3200933f5225c
TOMAR-PARAKRAM <tomarparakram17@gmail.com>
2025-10-09 23:58:26 +0530

    this code is working but the ui is not updating

---
+++ REACT/05counters/src/App.jsx
@@ -0,0 +1,34 @@
+import './App.css'
+
+function App() {
+
+let counter = 10 ; 
+
+const addValue = () => {
+  console.log("add value",(Math.random()*10));
+  counter = counter+1 ; 
+  console.log("dfsf");
+  
+}
+
+
+
+  return (
+    <>
+      <h1> MY MISERABLE MIND </h1>
+      <h3> counter value = {counter}</h3>
+
+      <button
+        onClick={addValue}>ADD VAL 
+      </button> 
+      <br></br>
+      <br></br>
+      <button>DEC VAL </button>
+
+
+
+    </>
+  )
+}
+
+export default App
