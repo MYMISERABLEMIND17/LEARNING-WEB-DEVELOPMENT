e1b86f7669685f373555b4c14071017e02d0b86b
TOMAR-PARAKRAM <tomarparakram17@gmail.com>
2025-10-11 19:49:22 +0530

    making the first project

---
+++ REACT/REACT-SHERIYANS/day1/import-export/app.js
@@ -0,0 +1,5 @@
+const a = 100 ; 
+
+export default a ; // this will export the variable 
+
+export const myName = "mmy" ; 
---
+++ REACT/REACT-SHERIYANS/day1/import-export/script.js
@@ -0,0 +1,6 @@
+import a from './app.js'
+import {myName} from './app.js';
+console.log(a);
+
+console.log(myName) ; 
+
