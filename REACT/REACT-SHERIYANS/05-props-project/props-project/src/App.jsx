e13866d81958da2b4e86d59c34a4ceabdee7750d
TOMAR-PARAKRAM <tomarparakram17@gmail.com>
2025-10-13 23:01:45 +0530

    a small project to practice props

---
+++ REACT/REACT-SHERIYANS/05-props-project/props-project/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className='parent'>
+        <h1>hello world</h1>
+        
+      </div>
+    </>
+  )
+}
+
+export default App
