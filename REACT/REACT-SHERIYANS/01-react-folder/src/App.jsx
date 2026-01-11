a4f0f105a5b02757ab86da7dd23857691feb8b37
MYMISERABLEMIND17 <tomarparakram17@gmail.com>
2026-01-11 15:35:44 +0530

    revise first topic

---
+++ REACT/REACT-SHERIYANS/01-react-folder/src/App.jsx
@@ -0,0 +1,15 @@
+
+  import React from 'react'
+
+const App = () => {
+  return (
+    <>      
+      <div>
+        <h1>hi this is my first app </h1>
+        <h2> follow me on </h2>
+      </div>
+    </>
+  )
+}
+
+export default App
