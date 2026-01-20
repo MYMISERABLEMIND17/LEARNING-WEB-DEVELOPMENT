77160253f8f8a814d08f1e6426732f9ed0eec0ae
MYMISERABLEMIND17 <tomarparakram17@gmail.com>
2026-01-21 05:02:37 +0530

    complete undeerstanding of the functions

---
+++ REACT/REACT-SHERIYANS/COMPLETED TOPICS/08-functions/src/App.jsx
@@ -0,0 +1,19 @@
+
+function App() {
+  function btnclick(){
+    console.log("button clicked");
+  }
+  return (
+    <>
+      <button onClick={btnclick}> click me</button>
+      <input type="text" placeholder="type here"
+        onChange={function(prop){
+          console.log(prop.target.value);
+        }}></input>
+
+        
+    </>
+  )
+}
+
+export default App
