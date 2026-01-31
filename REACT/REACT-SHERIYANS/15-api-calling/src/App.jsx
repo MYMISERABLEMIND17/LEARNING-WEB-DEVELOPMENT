2b5eebfbe340b1e249afd33983b6055e2ab13ea4
MYMISERABLEMIND17 <tomarparakram17@gmail.com>
2026-01-31 18:08:25 +0530

    complete the simple api calling concept + use async function

--- REACT/REACT-SHERIYANS/15-api-calling/src/App.jsx
+++ REACT/REACT-SHERIYANS/15-api-calling/src/App.jsx
@@ -1,33 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import axios from 'axios'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // this is async function 
+  // async function getData(){
+  //   const resp =  await fetch('https://official-joke-api.appspot.com/random_joke')
+  //   console.log(resp);
+  // }
+
+  const getData = async () =>{
+    const resp  =await axios.get('https://official-joke-api.appspot.com/random_joke')
+    console.log(resp.data.setup);
+    console.log(resp.data.punchline);
+    
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button onClick={getData}>tell me a joke </button>
+    
     </>
   )
 }
