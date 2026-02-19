ebb12f346d87cbd724d016323f8f62d28ebe1ea4
MYMISERABLEMIND17 <tomarparakram17@gmail.com>
2026-02-19 15:47:41 +0530

    lecture 3  namaste nodejs

--- NODE JS/LECTURES/lecture-1/script.js
+++
@@ -1,9 +0,0 @@
-// for each 
-var arr = [1, 3, 5, 7]
-arr.forEach(function(val){
-    console.log(val + "hello");
-});
-
-// map 
-x  = [2, 4, 6, 8 ]
-x.map(value, start1, stop1, start2, stop2)  
---
+++ NODE JS/app.js
@@ -0,0 +1,7 @@
+// var name =  " hello  whats up ";  
+// const ans = " mmy "
+
+// console.log(name);
+// console.log(ans); 
+
+console.log(globalThis);
