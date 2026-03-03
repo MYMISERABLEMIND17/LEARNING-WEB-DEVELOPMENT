e75ba7eda9a7265c858e42c287bd9f9903aec870
MYMISERABLEMIND17 <tomarparakram17@gmail.com>
2026-03-03 15:04:31 +0530

    learn and test get put post methods , through postman

--- NODE JS/SEASON2/DEV TINDER PROJECT /src/app.js
+++ NODE JS/SEASON2/DEV TINDER PROJECT /src/app.js
@@ -3,9 +3,24 @@ const express = require("express")
 const app = express() ; 
 
 app.listen(3000 , ()=> {
-  console.log("hello the server is running ");
-  
+  console.log("hello the server is running ");  
 })
+
+app.get("/home/profile1" , (req , res) => {
+  res.send({name: "tom " , age : 33 }  + "this is post call " ) ; 
+  res.send()
+})
+
+app.post("/home/profile1" , (req , res) => {
+  res.send("this is the post call ")
+})
+
+app.put("/home/profile1" , (req , res) =>{
+  res.send(" this is the put call ")
+})
+
+
+
 app.use("/home/profile1" , (req , res) => {
   res.send("this is the profile 1 ") ; 
 })
